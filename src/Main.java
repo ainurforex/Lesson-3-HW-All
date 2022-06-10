03af3c16af070202c3a3a395a4d4c8c80f7adf7e
Ainur Sharipov <ainurforex@mail.ru>
2022-06-10 22:37:07 +0500

    Урок 3 1-7 задачи fix

--- src/Main.java
+++ src/Main.java
@@ -44,66 +44,49 @@ public class Main {
     public static  void homeWork3(){
         int year=2021;
         System.out.println("Home work 3");
-        if (year%4==0){
-            if (year%100!=0||year%400==0){
+        if (year%4==0&&year%100!=0||year%400==0){
                 System.out.println(year+" год является високосным");
-            }else System.out.println(year+" год не является високосным");
         }else System.out.println(year+" год не является високосным");
         System.out.println("----------------------------------------------------------------");
     }
 
     public static  void homeWork4(){
         int deliveryDistance = 95;
-        int deliveryDays;
+        int deliveryDays=1;
         System.out.println("Home work 4");
-        if (deliveryDistance<20){
-            deliveryDays=1;
-            System.out.println("Потребуется дней: " + deliveryDays);
-        }else {
-            deliveryDays=(int)Math.ceil((double) (deliveryDistance-20)/40.0)+1;
-            System.out.println("Потребуется дней: " + deliveryDays);
+        if (deliveryDistance>20) {
+            deliveryDays++;
         }
+        if (deliveryDistance>60) {
+            deliveryDays++;
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
+
         System.out.println("----------------------------------------------------------------");
     }
     public static  void homeWork5(){
-        int monthNumber = 11;
+        int monthNumber = 13;
         String season;
         System.out.println("Home work 5");
 
         switch (monthNumber) {
             case (1):
-                season="Зима";
-                break;
             case (2):
-                season="Зима";
-                break;
             case (12):
                 season="Зима";
                 break;
             case (3):
-                season="Весна";
-                break;
             case (4):
-                season="Весна";
-                break;
             case (5):
                 season="Весна";
                 break;
             case (6):
-                season="Лето";
-                break;
             case (7):
-                season="Лето";
-                break;
             case (8):
                 season="Лето";
                 break;
             case (9):
-                season="Осень";
-                break;
             case (10):
-                season="Осень";
-                break;
             case (11):
                 season="Осень";
                 break;
@@ -121,7 +104,7 @@ public class Main {
 
         int age = 19;
         int salary = 58_000;
-        double koefLimit;
+        double koefLimit=1;
         int credit;
         System.out.println("Home work 6");
         if (salary>=50_000) {
@@ -129,7 +112,7 @@ public class Main {
         }
         if (salary>=80_000){
             koefLimit=1.5;
-        }else koefLimit=1;
+        }
         if(age<23){
             credit=(int)(salary*2*koefLimit);
         }else credit=(int)(salary*3*koefLimit);
